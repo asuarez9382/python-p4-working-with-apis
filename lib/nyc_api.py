3e56c325361fb52875a44c2355e03b477e0599b4
Adrian Suarez <asuarez9382@gmail.com>
2024-04-30 09:19:29 -0700

    complete lab

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -9,6 +9,15 @@ class GetPrograms:
 
     response = requests.get(URL)
     return response.content
+  
+  def program_agencies(self):
+    # we use the JSON library to parse the API response into nicely formatted JSON
+        programs_list = []
+        programs = json.loads(self.get_programs())
+        for program in programs:
+            programs_list.append(program["agency"])
+
+        return programs_list
 
 programs = GetPrograms().get_programs()
 print(programs)
